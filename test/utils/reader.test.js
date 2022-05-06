fefcd1806f6676dbb8ffed733d8500ff0d407809
Cristian <cris18002011@hotmail.com>
2022-05-05 19:15:55 -0500

    Mejorando testing de reader

--- test/utils/reader.test.js
+++ test/utils/reader.test.js
@@ -3,6 +3,24 @@ const reader = require("../../libs/utils/reader");
 describe("TDD for reader.js", () => {
 	test("1) Read json file", () => {
 		const visualPartners = reader.readJsonFile("visualPartners.test.json");
-		expect(visualPartners.length).toBe(51);
+		expect(visualPartners).toHaveLength(51);
+            expect(visualPartners[5].name).toBe("Taylor");
+		expect(typeof(visualPartners[5].credits) == "number").toBe(true);
+	});
+      test("2) Read json file complete", () => {
+		const visualPartners = reader.readJsonFile("visualPartners.test.json");
+		
+		for(let partner of visualPartners){
+                  expect(partner.name && partner.id &&
+				partner.email && partner.credits &&
+				partner.enrollments && partner.previousCourses &&
+				partner.haveCertification).not.toBeNull();
+			expect(partner.name && partner.id &&
+				partner.email && partner.credits &&
+				partner.enrollments && partner.previousCourses &&
+				partner.haveCertification).not.toBeUndefined();
+                  expect(typeof(partner.credits) == "number" &&
+				typeof(partner.previousCourses) == "number").toBe(true);
+            }
 	});
 });
